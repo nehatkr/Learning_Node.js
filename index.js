e647ef6ef5da12c2df5cb81ac759219811738152
Nehatkr <nehathakur2308@gmail.com>
2025-08-28 10:50:05 +0530

    practice

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("hey node js");
-console.log("We will do it");
+// console.log("hey node js");
+// console.log("We will do it");
+
+const lodash = require('lodash')
+
+const names = ['neha', 'rahul', 'shweta', 'rishika'];
+const capitalize = lodash.map(names, lodash.capitalize);
+
+console.log(capitalize);
+
